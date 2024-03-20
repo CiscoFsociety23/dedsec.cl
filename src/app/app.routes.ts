c2018733adc559b83c7ccad15dc42472dfc896f8
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-03-19 23:48:38 -0300

    fix: rename route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: 'catalog', loadComponent: () => import('@pages/catalog/catalog.component').then(comp => comp.CatalogComponent) },
+  { path: 'catalogue', loadComponent: () => import('@pages/catalog/catalog.component').then(comp => comp.CatalogComponent) },
   { path: 'categories', loadComponent: () => import('@pages/categories/categories.component').then(comp => comp.CategoriesComponent) },
   { path: 'contact', loadComponent: () => import('@pages/contact/contact.component').then(comp => comp.ContactComponent) },
   { path:"**", redirectTo:'' },
