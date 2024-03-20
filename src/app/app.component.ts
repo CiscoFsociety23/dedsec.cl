bace490a2f473ea40b928200c7200e3fcecc798c
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-03-19 23:44:36 -0300

    feat: web skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeLayoutComponent } from '@pages/home-layout/home-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
