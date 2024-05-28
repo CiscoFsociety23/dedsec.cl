a30802baf7cb080940d13b432363d555631a7532
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-05-28 18:32:31 -0400

    feat: adding corp seals

---
+++ src/app/components/corp-identity/corp-identity.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corp-identity',
+  standalone: true,
+  imports: [],
+  templateUrl: './corp-identity.component.html',
+  styleUrl: './corp-identity.component.css'
+})
+export class CorpIdentityComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { CorpIdentityComponent } from '@components/corp-identity/corp-identity.component';
 import { LoginComponent } from '@components/login/login.component';
 import { SessionManagerComponent } from '@components/session-manager/session-manager.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LoginComponent, SessionManagerComponent],
+  imports: [LoginComponent, SessionManagerComponent, CorpIdentityComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
