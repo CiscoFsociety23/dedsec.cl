dd8690ec12b3d7e30481b64200e6605caba84ef6
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-07-22 11:27:54 -0400

    feat: adding administration page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('@pages/home/home.component').then(comp => comp.HomeComponent) },
+  { path: 'administracion', loadComponent: () => import('@pages/administration/administration.component').then(comp => comp.AdministrationComponent) },
   { path: 'sobre-nosotros', loadComponent: () => import('@pages/about-us/about-us.component').then(comp => comp.AboutUsComponent) },
   { path:"**", loadComponent: () => import('@pages/not-found/not-found.component').then(comp => comp.NotFoundComponent) }
 ];
