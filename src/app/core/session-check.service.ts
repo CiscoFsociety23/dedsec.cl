46ffbcd2184092fced72b80cf7ea6f4bd2a0b96a
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-07-22 13:30:37 -0400

    feat: adding auth control

---
+++ src/app/core/session-check.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { env } from '@environment';
+import { Validity } from '@interfaces/auth';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class SessionCheckService {
+
+  constructor(private http: HttpClient){}
+
+  public checkValidity(token: string): Observable<Validity> {
+    const auth: HttpHeaders = new HttpHeaders({'Authorization': token});
+    return this.http.post<Validity>(`${env.URL_API_JUPITER}/api-jupiter/auth/verify`, null, { headers: auth });
+  };
+
+  public checkToken(): boolean {
+    const token: string = localStorage.getItem('auth') || '!';
+    if(token != '!'){
+      return true;
+    } else {
+      return false;
+    };
+  };
+
+}
--- src/app/pages/administration/administration.component.ts
+++ src/app/pages/administration/administration.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NotFoundComponent } from '@pages/not-found/not-found.component';
+import { SessionCheckService } from '@services/session-check.service';
 
 @Component({
   selector: 'app-administration',
   standalone: true,
-  imports: [],
+  imports: [NotFoundComponent],
   templateUrl: './administration.component.html',
   styleUrl: './administration.component.css'
 })
-export class AdministrationComponent {
+export class AdministrationComponent implements OnInit {
+  
+  constructor(private http: HttpClient) {}
+  
+  private tokenChecker: SessionCheckService = new SessionCheckService(this.http);
+
+  ngOnInit(): boolean {
+    return this.tokenChecker.checkToken()
+  };
 
 }
