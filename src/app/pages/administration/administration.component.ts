715c990c54fd18118a84ff4ecc922fcb57ee1059
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-07-26 23:52:18 -0400

    feat: adding form for user registry

--- src/app/pages/administration/administration.component.ts
+++ src/app/pages/administration/administration.component.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AdminPanelComponent } from '@components/admin-panel/admin-panel.component';
+import { UserRegistryComponent } from '@components/user-registry/user-registry.component';
 import { NotFoundComponent } from '@pages/not-found/not-found.component';
 import { SessionCheckService } from '@services/session-check.service';
 
 @Component({
   selector: 'app-administration',
   standalone: true,
-  imports: [ AdminPanelComponent, NotFoundComponent],
+  imports: [ AdminPanelComponent, NotFoundComponent, UserRegistryComponent ],
   templateUrl: './administration.component.html',
   styleUrl: './administration.component.css'
 })
