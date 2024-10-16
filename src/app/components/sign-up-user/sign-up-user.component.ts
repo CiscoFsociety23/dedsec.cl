480b68054cc79305ae92a0e2d4ef4592e408f055
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-10-16 18:42:46 -0300

    feat: adding component

---
+++ src/app/components/sign-up-user/sign-up-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './sign-up-user.component.html',
+  styleUrl: './sign-up-user.component.css'
+})
+export class SignUpUserComponent {
+
+}
