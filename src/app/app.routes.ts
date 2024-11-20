c6d6ca736c6dfe6d3f3d4f77029d8f116b46e1aa
 Marco Figueroa <marcofigueroa23@icloud.com>
2024-11-20 12:19:17 -0300

    feat: adding new component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ export const routes: Routes = [
   { path: '', loadComponent: () => import('@pages/home/home.component').then(comp => comp.HomeComponent) },
   { path: 'administracion', loadComponent: () => import('@pages/administration/administration.component').then(comp => comp.AdministrationComponent) },
   { path: 'sobre-nosotros', loadComponent: () => import('@pages/about-us/about-us.component').then(comp => comp.AboutUsComponent) },
+  { path: 'politica-privacidad', loadComponent: () => import('@components/privacy-policy/privacy-policy.component').then(comp => comp.PrivacyPolicyComponent) },
   { path: 'no-validado', loadComponent: () => import('@components/no-validated/no-validated.component').then(comp => comp.NoValidatedComponent) },
   { path: 'registry', loadComponent: ()=> import('@components/sign-up-user/sign-up-user.component').then(comp => comp.SignUpUserComponent) },
   { path: 'registry/send-validation', loadComponent: () => import('@components/req-validation/req-validation.component').then(comp => comp.ReqValidationComponent) },
