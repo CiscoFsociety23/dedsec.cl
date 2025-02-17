a72849942db8808f7cdef091084409a3ea8e9167
 Marco Figueroa <marcofigueroa23@icloud.com>
2025-02-17 02:35:23 -0300

    feat: adding env file

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  URL_API_MARS: process.env['URL_API_MARS'] || '',
+  URL_API_JUPITER: process.env['URL_API_JUPITER'] || '',
+  URL_API_VENUS: process.env['URL_API_VENUS'] || '',
+  TOKEN_ADMIN: process.env['TOKEN_ADMIN'] || ''
+};
