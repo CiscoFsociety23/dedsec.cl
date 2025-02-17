126e431d212caacfff65b7c13bdd31acedd371fc
 Marco Figueroa <marcofigueroa23@icloud.com>
2025-02-17 03:05:15 -0300

    fix: modify script and env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  URL_API_MARS: '',
-  URL_API_JUPITER: '',
-  URL_API_VENUS: '',
+  URL_API_MARS: 'https://mars.dedsec.cl/api-mars',
+  URL_API_JUPITER: 'https://jupiter.dedsec.cl/api-jupiter',
+  URL_API_VENUS: 'https://venus.dedsec.cl/api-venus',
   TOKEN_ADMIN: ''
 };
